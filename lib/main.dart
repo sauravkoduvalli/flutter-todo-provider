197a8466ad83eb32e1823edd74cb2f235ece95c6
Saurav K <sauravkoduvalli@gmail.com>
2024-08-27 17:23:13 +0530

    few platforms removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:todo_provider/screens/home_screen.dart';
+import 'package:todo_provider/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
